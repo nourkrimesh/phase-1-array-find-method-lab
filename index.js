c844bdfe899404996b5c941ffe8cd2c4ea5ed7af
Nour Krimesh <nour.krimesh@gmail.com>
2023-07-05 21:53:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+
+function superbowlWin(records) {
+    const record = records.find(record => record.result === 'W');
+    return record === undefined ? undefined : record.year;
+}
